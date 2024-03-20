e327c0cf83b2b0b28092c8aa29b10d6fced5c0ad
mrmalik7844 <161038986+mrmalik7844@users.noreply.github.com>
2024-03-20 11:46:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,38 +3,8 @@ import Home from "./components/home/Home";
 import ProductPage from "./components/product/ProductPage";
 import CheckoutPage from "./components/checkout/CheckoutPage";
 import DownloadApp from "./components/reusable/DownloadApp";
-import React, { useEffect } from "react";
 function App() {
 
-    useEffect(() => {
-    function isFacebookApp() {
-        console.log("code test");
-      var ua = navigator.userAgent || navigator.vendor || window.opera;
-      return ua.indexOf("FBAV") > -1 || ua.indexOf("FBAN") > -1;
-    }
-
-    if (isFacebookApp()) {
-      // eslint-disable-next-line no-restricted-globals
-      var currentLink = location.href;
-      if (currentLink.indexOf("https") > -1) {
-        currentLink = currentLink.replace("https://", "");
-        currentLink = currentLink.replace("www.", "");
-        var chromeLink =
-          "intent://" +
-          currentLink +
-          "#Intent;scheme=https;package=com.android.chrome;end";
-        window.location.href = chromeLink;
-      }
-      if (currentLink.indexOf("http") > -1) {
-        currentLink = currentLink.replace("http://", "");
-        currentLink = currentLink.replace("www.", "");
-        chromeLink =
-          "intent://" +
-          currentLink +
-          "#Intent;scheme=http;package=com.android.chrome;end";
-        window.location.href = chromeLink;
-      }
-    }
   }, []);
   
   return (
